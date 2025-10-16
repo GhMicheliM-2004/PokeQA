139e438bcfa040c3fa787e5c5bbdf5d97f0ef2ec
GhMicheliM-2004 <michelimghs@gmail.com>
2025-10-16 16:16:26 -0300

    Update services and app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,35 @@
 # Inicializa o servidor web Flask
+from flask import Flask, make_response, jsonify
+# from services.pokeQA_service import busca_pokemon_nome, busca_pokemon_id, busca_info_pokemon
+from flask import render_template
+from teste import data
+
+app = Flask(__name__, template_folder='../templates') # Define o diretório dos templates HTML
+
+# Endpoint da página inicial
+@app.route('/')
+def get_index():
+    return render_template("index.html") # Renderiza o template index.html
+
+# Endpoint para retornar os dados do arquivo teste.py
+@app.route('/pokemon', methods=['GET'])
+def get_pokemon():
+    return make_response(
+        jsonify(data)
+    )
+
+
+def main():
+    app.run(debug=True)
+    # nome = input("Digite o nome do Pokémon: ")
+    # if nome():
+    #     resultado_nome = busca_pokemon_nome(nome)
+    #     print(f"Resultado da busca por nome ({nome}):")
+    # else:
+    #     pokemon_id = input("Digite o ID do Pokémon: ")
+    #     resultado_nome = busca_pokemon_id(pokemon_id)
+    #     print(f"\nResultado da busca por ID ({pokemon_id}):")
+    # print(resultado_nome)
+
+if __name__ == "__main__":
+    main()
--- backend/services/pokeQA_service.py
+++ backend/services/pokeQA_service.py
@@ -1,1 +1,56 @@
 # Responsável pela conexão com a API do PokeQA
+# Aqui vai os serviços que fazem a ponte entre o Flask e o PokeQA
+# Requests para a API do PokeQA
+
+import requests
+
+def busca_pokemon_nome(nome):
+    url = f"https://pokeapi.co/api/v2/pokemon/{nome}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        response_json = response.json()
+        response_infos = busca_info_pokemon(response_json)
+        return response_infos
+    else:
+        return {"error": "Pokémon não encontrado"}
+
+def busca_pokemon_id(pokemon_id):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        response_json = response.json()
+        response_infos = busca_info_pokemon(response_json)
+        return response_infos
+    else:
+        return {"error": "Pokémon não encontrado"}
+
+def busca_info_pokemon(response_json):
+    nome = response_json.get("name")
+    id_pokemon = response_json.get("id")
+    tipos = [t['type']['name'] for t in response_json.get("types")]
+    stats = [{stat['stat']['name']: stat['base_stat'] for stat in response_json.get("stats", [])}]
+    
+    return {
+        "nome": nome,
+        "id": id_pokemon,
+        "tipos": tipos,
+        "stats": stats
+    }
+
+def main():
+    # Testa a função de busca por nome
+    nome = input("Digite o nome do Pokémon: ")
+    resultado_nome = busca_pokemon_nome(nome)
+    print(f"Resultado da busca por nome ({nome}):")
+    print(resultado_nome)
+    
+    # Testa a função de busca por ID
+    pokemon_id = input("\nDigite o ID do Pokémon: ")
+    resultado_id = busca_pokemon_id(pokemon_id)
+    print(f"\nResultado da busca por ID ({pokemon_id}):")
+    print(resultado_id)
+
+if __name__ == "__main__":
+    main()
